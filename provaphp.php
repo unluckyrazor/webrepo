60f2cab9017e339912fb434fa8c0889568dc66f3
unluckyrazor <giuseppe.patane@outlook.com>
2024-05-05 17:33:37 +0200

    resapi spoty provs

---
+++ provaphp.php
@@ -0,0 +1,5 @@
+<?php 
+    $argomento = 'php';
+    echo "l'argomento della lezione di oggi è" . $argomento . "!";
+    echo "l'argomento della lezione di oggi è " . $argomento . "!";
+    ?>
--- sito/sito.js
+++ sito/sito.js
@@ -113,3 +113,208 @@ cambiare una o più immagini della pagina quando l’utente clicca o si muove so
 aggiungere una classe “preferito” (con conseguenti modifiche estetiche definite in CSS) quando l’utente clicca su certi elementi della pagina;
 conservare informazioni negli attributi data-*, da usare per mostrare dati aggiuntivi quando l’utente clicca o si muove sopra certi elementi.
 */
+
+
+
+/* prova api
+
+chiamata api rest igdb 
+
+const client_id= '4xc9gc11f5vtpv15695cb46blzrf3k';
+const client_secret = 'dcxht65j1e3t9cq1xuwsft71l310eh'
+let token='';
+
+function onTokenResponse(response){
+    console.log("token ritornato");
+    if (!response.ok) {
+        console.log("errore dentro")
+        
+    }
+    return response.json();
+    
+    
+}
+
+function onTokenJson(json){
+    console.log("json tornato");
+    console.log(json);
+    if (!json) {
+        console.log("errore json non funziona\n")
+    }
+    token=json.access_token;
+    console.log(token);
+    
+}
+
+function onErrorResponse(failure) {
+    console.log("fallito")
+}
+
+function onResponse(response) {
+    console.log("response ritornato 2")
+    if(!response.ok) {
+        console.log("errore non ok")
+    }
+    return response.json();
+}
+function onJson(json){
+    console.log("json ritornato 2")
+    console.log(json);
+}
+function onError(error) {
+    console.log(error);
+    console.log("errore");
+}
+
+fetch("https://id.twitch.tv/oauth2/token", {
+    method: "post",
+    body: "client_id=4xc9gc11f5vtpv15695cb46blzrf3k&client_secret=dcxht65j1e3t9cq1xuwsft71l310eh&grant_type=client_credentials",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+ }
+ }).then(onTokenResponse, onErrorResponse).then(onTokenJson);
+
+
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games", {
+    method: "post",
+    
+    body: "fields *;",
+    headers: {
+        "Client-ID": client_id,
+        "Authorization": "Bearer" + token
+ }
+ }).then(onResponse, onError).then(onJson);
+*/
+
+ //non posso farlo perche mi da corse error e dovrei fare richieste dal server o da  un proxy icsdi
+ 
+
+
+ // prova api spotify 
+ const spotify_client_id='5510ef73fb5348c8b8dec19039f1dc9d';
+ const spotify_client_secret='2b9a114862484c86bc8fb75c8406fb52';
+
+
+ let token_spotify='';
+ let album_value='League'
+ let spoty_content;
+  
+ 
+
+function onTokenResponse(response){
+    console.log("token response ritornato");
+    if (!response.ok) {
+        console.log("errore dentro")
+        
+    }
+    return response.json();
+    
+    
+}
+function onTokenJson(json){
+    console.log("json token tornato");
+    
+    if (!json) {
+        console.log("errore json non funziona\n")
+    }
+    token_spotify=json.access_token;
+    
+    
+    prendi();
+    
+    
+}
+function onResponse(response){
+    console.log("response contenuto tornato");
+    return response.json();
+    
+}
+function onJson(json){
+    console.log("json contenuto tornato");
+    
+    
+    return json;
+}
+
+
+function selectTracks(playlist) {
+    const selectedTracks = playlist.items.slice(0, 5); // prende le prime 5
+    console.log('Selected tracks:', selectedTracks);
+
+    const tracklist= document.querySelectorAll(".track_name");
+    const tracklinklist=document.querySelectorAll(".spoty_track_link");
+    const trackimageList = document.querySelectorAll(".spoty_track_image");
+    
+
+    let i=0;
+        
+    for (let trackObj of selectedTracks) {
+        
+        
+        console.log(selectedTracks[i])
+        let trackLink = trackObj.track.external_urls.spotify;
+        let trackName = trackObj.track.name;
+        let trackImage = trackObj.track.album.images[1].url;
+        console.log("track name", trackName);
+        
+        
+        trackimageh=trackimageList[i];
+        track=tracklist[i];
+        tracklinkh=tracklinklist[i];
+        
+
+        trackimageh.src=trackImage;
+        track.textContent=trackName;
+        tracklinkh.href=trackLink;
+        
+        i++;
+        
+        }
+    
+    
+    }
+        
+    
+
+function HandlePlaylist(playlists) {
+    // sceglie la playlist 0
+    console.log(playlists.playlists)
+    
+    if (!playlists.playlists || !playlists.playlists.items || playlists.playlists.items.length === 0) {
+        console.error('non ho trovato  playlist');
+        return;
+    }
+
+    const playlistId = playlists.playlists.items[0].id;
+    // fetcha tracce
+    fetch('https://api.spotify.com/v1/playlists/'+ playlistId + '/tracks', {
+        headers: {
+            "Authorization": "Bearer " + token_spotify
+        }
+    })
+    .then(response => response.json())
+    .then(selectTracks)
+    ; 
+}
+
+
+// ritorna il contenuto che mi interessa
+function prendi() { fetch("https://api.spotify.com/v1/browse/categories/gaming/playlists" ,
+{
+    headers:{
+        "Authorization": "Bearer " + token_spotify
+    }
+}).then(onResponse).then(onJson).then(HandlePlaylist); }
+
+// fetcha il token che mi serve
+fetch("https://accounts.spotify.com/api/token", {
+    method: "post",
+    body: "grant_type=client_credentials",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + btoa(spotify_client_id + ":" + spotify_client_secret)
+ }
+ }).then(onTokenResponse).then(onTokenJson);
+
+//  scrivere il codice che cambia src delle immagini della top, cambia il titolo della canzone, e cambia il link
